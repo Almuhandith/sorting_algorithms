9348a7192076d912bd1dbfdacc8cff0075dec80d
Abdulsamad <almuhandith1497@gmail.com>
2023-01-25 11:11:28 +0100

    ch: bubble sort fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,12 +10,13 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	int temp;
-	size_t i;
-	size_t j;
+	size_t i, j, temp, flag;
+	if (!array || size < 2)
+		return;
 
 	for (i = 0; i < size; i++)
 	{
+		flag = 0;
 		for (j = 0; j < (size - 1); j++)
 		{
 			if (array[j] > array[j + 1])
@@ -23,8 +24,11 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				flag = 1;
 				print_array(array, size);
 			}
 		}
+		if (flag == 0)
+			break;
 	}
 }
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts a doubly linked list
+ * according to insertion sort algorithm
+ *
+ * @list: list to be sorted
+ * Return: Nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+     
+}
